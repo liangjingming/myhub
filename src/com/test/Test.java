7ba360f11d78010bf7faaad51eb306a8af47d896
liangjingming <liangjingming@xingshulin.com>
2016-04-25 15:02:32 +0800

    1

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -2,7 +2,7 @@ package com.test;
 
 public class Test {
 
-	public static3 void main(String args[]) {
+	public static4 void main(String args[]) {
 		SayHiEmlement test = new SayHiEmlement();
 		test.SayHiAnnotation("");;
 	}
