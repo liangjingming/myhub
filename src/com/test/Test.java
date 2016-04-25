43fa5304160936c387fb3b9321c950031b17d4b5
liangjingming <liangjingming@xingshulin.com>
2016-04-25 22:42:11 +0800

    1

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -2,8 +2,8 @@ package com.test;
 
 public class Test {
 
-	public static8 void main(String args[]) {
-		System.out.println();
+	public static9 void main(String args[]) {
+		System.out.println(1);
 		SayHiEmlement test = new SayHiEmlement();
 		test.SayHiAnnotation("");;
 	}
