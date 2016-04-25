cb3c451e616e7c10258f6a24544439e94154013d
liangjingming <liangjingming@xingshulin.com>
2016-04-25 16:15:42 +0800

    modify

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -2,7 +2,7 @@ package com.test;
 
 public class Test {
         public void test(){}
-	public static4 void main(String args[]) {
+	public static6 void main(String args[]) {
 		SayHiEmlement test = new SayHiEmlement();
 		test.SayHiAnnotation("");;
 	}
